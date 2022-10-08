403ceed545fbe881172f474a5b8cbd263e9d9632
karvalho <renato@karvalho.com.br>
2022-10-08 17:26:31 -0300

    first commit

---
+++ Source/HorrorGame/HorrorGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HorrorGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AHorrorGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
